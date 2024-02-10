6e242801bca7b7efec106976a1443b85d18eb530
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-02-10 13:10:22 +0200

    clone project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import React from 'react';
 import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        TodoList
-      </Provider>
+    <div className="App">
+      <Header />
+      <main className="main">
+        <div className="main__container">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
